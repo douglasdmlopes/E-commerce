65b9c6ccc82428d91b6ed00940451c13ccb0adbc
Douglas Lopes <douglasdmlopes@gmail.com>
2020-07-11 11:20:50 -0300

    Fix colors

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import 'antd/dist/antd.css';
+import './styles/meutema.css';
 import '@brainhubeu/react-carousel/lib/style.css';
 import 'semantic-ui-css/semantic.min.css';
 
